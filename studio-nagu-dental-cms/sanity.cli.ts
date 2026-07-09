cac1e4c423842ecbf038cac235fa98db65d170de
Praveen-Kadraiah <praveen@nagudental.in>
2026-07-09 18:47:38 +0530

    chore: configure studioHost domain in sanity.cli.ts

--- studio-nagu-dental-cms/sanity.cli.ts
+++ studio-nagu-dental-cms/sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'exi81qhl',
     dataset: 'production'
   },
+  studioHost: 'nagu-dental-hub',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
